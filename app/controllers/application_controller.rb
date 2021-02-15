5a106abc80dcd856567b7ce91e685e356f8e53b2
le-santos <lequinterio@gmail.com>
2021-02-15 20:53:37 -0300

    candidate creates a profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  after_action :store_action
+  
+  def store_action
+    return unless request.get? 
+    if (request.path != "/candidates/sign_in" &&
+        request.path != "/candidates/sign_up" &&
+        request.path != "/candidates/password/new" &&
+        request.path != "/candidates/password/edit" &&
+        request.path != "/candidates/confirmation" &&
+        request.path != "/candidates/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      store_location_for(:candidate, request.fullpath)
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
--- spec/features/candidate_creates_profile_spec.rb
+++ spec/features/candidate_creates_profile_spec.rb
@@ -43,6 +43,44 @@ feature 'A visitor creates a candidate profile' do
     expect(page).to have_link('Sair')
   end
 
+  scenario 'and returns to job application page' do
+    company = Company.create!(name: 'Atendbots' , 
+                              description: 'Sistemas de automação de atendimento (chatbots) para pequenos negócios' , 
+                              logo: 'atendbot_url' , 
+                              address: 'Rua dos devs, 101, São Paulo, SP', 
+                              tech_stack: 'HTML, Ruby, Ruby on Rails, Bootstrap', 
+                              domain: 'www.atendbots.com.br')
+                    
+    job = Job.create!(title: 'Desenvolvedor(a) Backend Júnior', 
+                      details: 'Desenvolvedor(a) Ruby on Rails para desenvolvimento de aplicações web', 
+                      salary: 3500, 
+                      level: 'Júnior', 
+                      requirements: 'Ruby on Rails, SQLite, HTML, CSS, Bootstrap, Git, TDD', 
+                      deadline: '24/12/2022', 
+                      quantity_of_positions: 4, 
+                      company: company )
+
+    visit job_path(job)
+    click_on 'Candidatar-se'
+    click_on 'Criar conta'
+    fill_in 'Nome', with: 'Paco Silva'
+    fill_in 'E-mail', with: 'paco@gmail.com'
+    fill_in 'Senha',  with: '123456'
+    fill_in 'Confirme sua senha', with: '123456'
+    fill_in 'CPF', with: '123.456.789-00'
+    fill_in 'Fone', with: '11 91234-5678'
+    fill_in 'Biografia', with: 'Estudante de Ciência da Computação'
+    fill_in 'Habilidades', with: 'Ruby on Rails, HTML'
+    click_on 'Inscrever-se'
+
+    expect(current_path).to eq(job_path(job))
+    expect(page).to have_content(company.name)
+    expect(page).to have_content(job.title)
+    expect(page).not_to have_link('Candidatar-se')
+    expect(page).to have_link('Enviar Candidatura')
+
+  end
+
   scenario 'and email must be unique' do
     company = Company.create!(name: 'Atendbots' , 
                               description: 'Sistemas de automação de atendimento (chatbots) para pequenos negócios' , 
