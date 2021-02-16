01ea242ebe36060916d8c34cc9911d9995deeb30
le-santos <lequinterio@gmail.com>
2021-02-16 08:59:08 -0300

    candidate can sign out

--- spec/features/candidate_creates_profile_spec.rb
+++ spec/features/candidate_creates_profile_spec.rb
@@ -148,4 +148,39 @@ feature 'A visitor creates a candidate profile' do
     expect(page).to have_content('Biografia não pode ficar em branco')
     expect(page).to have_content('Habilidades não pode ficar em branco')
   end
+
+  scenario 'and can sign out' do 
+    company = Company.create!(name: 'Atendbots' , 
+                              description: 'Sistemas de automação de atendimento (chatbots) para pequenos negócios' , 
+                              logo: 'atendbot_url' , 
+                              address: 'Rua dos devs, 101, São Paulo, SP', 
+                              tech_stack: 'HTML, Ruby, Ruby on Rails, Bootstrap', 
+                              domain: 'www.atendbots.com.br')
+                    
+    job = Job.create!(title: 'Desenvolvedor(a) Backend Júnior', 
+                      details: 'Desenvolvedor(a) Ruby on Rails para desenvolvimento de aplicações web', 
+                      salary: 3500, 
+                      level: 'Júnior', 
+                      requirements: 'Ruby on Rails, SQLite, HTML, CSS, Bootstrap, Git, TDD', 
+                      deadline: '24/12/2022', 
+                      quantity_of_positions: 4, 
+                      company: company )
+
+    visit job_path(job)
+    click_on 'Candidatar-se'
+    click_on 'Criar conta'
+    fill_in 'Nome', with: 'Paco Silva'
+    fill_in 'E-mail', with: 'paco@gmail.com'
+    fill_in 'Senha',  with: '123456'
+    fill_in 'Confirme sua senha', with: '123456'
+    fill_in 'CPF', with: '123.456.789-00'
+    fill_in 'Fone', with: '11 91234-5678'
+    fill_in 'Biografia', with: 'Estudante de Ciência da Computação'
+    fill_in 'Habilidades', with: 'Ruby on Rails, HTML'
+    click_on 'Inscrever-se'
+    click_on 'Sair'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content('Logout efetuado com sucesso.')
+  end
 end
