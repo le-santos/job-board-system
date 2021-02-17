18817d544dbdba51eeeedeaa89ee5956bff4b53e
le-santos <lequinterio@gmail.com>
2021-02-17 18:39:27 -0300

    add validation for uniqueness of JobApplication

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -1,4 +1,6 @@
 class JobApplication < ApplicationRecord
   belongs_to :candidate
   belongs_to :job
+
+  validates :candidate, uniqueness: { scope: :job, message: 'já se candidatou para a vaga' }
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -8,12 +8,21 @@ RSpec.describe Job, type: :model do
     
       job_app = job.applyForJob!(candidate)
 
+
       expect(candidate.job_applications.size).to eq(1)
       expect(candidate.job_applications.last).to eq(job_app)
     end
 
-    #it 'should not apply if JobApplication already exists' do
-    
-    #end 
+    it 'can not apply if JobApplication already exists' do
+      candidate = FactoryBot.create(:candidate)
+      job = FactoryBot.create(:job)
+      JobApplication.create!(candidate: candidate, job: job)
+      
+      expect {
+        job.applyForJob!(candidate)
+      }.to raise_error(ActiveRecord::RecordInvalid)
+      
+      expect(candidate.job_applications.size).to eq(1)
+    end 
   end
 end
