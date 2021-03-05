fbf8d7d58d15471be0d47fb813a4d24d3f7c21f6
le-santos <lequinterio@gmail.com>
2021-03-05 11:24:58 -0300

    adds bootstrap to job index and link to job details

--- spec/features/visitor_views_job_list_spec.rb
+++ spec/features/visitor_views_job_list_spec.rb
@@ -23,6 +23,21 @@ feature 'Visitor views list jobs' do
     click_on 'Vagas'
 
     expect(page).to have_content('Nenhuma vaga disponível no momento')
-    expect(page).not_to have_css("section.job-list")
+    expect(page).not_to have_css("div.job-details")
+  end
+
+  scenario 'and can access job details page' do
+    employee = FactoryBot.create(:company )
+    job_front = FactoryBot.create(:job, { title: 'Desenvolvedor(a) Frontend'} )
+    job_back = FactoryBot.create(:job, { title: 'Desenvolvedor(a) Backend'} )
+    
+    visit root_path
+    click_on 'Vagas'
+    within("div.job-details-#{job_front.id}") do
+      click_on "Detalhes da Vaga"
+    end
+
+    expect(current_path).to eq(job_path(job_front))
+    expect(page).to have_content(job_front.title)
   end
 end
