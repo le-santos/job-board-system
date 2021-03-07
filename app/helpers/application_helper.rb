5278a83f82a2c764ddebd57f284161537efc8105
le-santos <lequinterio@gmail.com>
2021-03-07 17:02:18 -0300

    adds style to login forms using shared partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
   def bootstrap_class_for_flash(flash_type)
     case flash_type
     when 'success'
-      'alert-success'
+      'alert-success p-2'
     when 'error'
-      'alert-danger'
+      'alert-danger p-2'
     when 'alert'
-      'alert-warning'
+      'alert-warning p-2'
     when 'notice'
-      'alert-info'
+      'alert-info p-2'
     else
       flash_type.to_s
     end
